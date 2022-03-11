13a32257fc6972340fd07f51ac6e03334ae89c3b
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-11 22:29:59 +0900

    ➕ CLEAKをインストール

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: { TMDB_API_KEY: "baa7af1f304682eb49bcbb6db49c1579" },
+  images: { domains: ["image.tmdb.org"] },
+  env: {
+    TMDB_API_KEY: "baa7af1f304682eb49bcbb6db49c1579",
+    CLERK__FRONTEND_API_KEY: "clerk.divine.vervet-45.lcl.dev",
+  },
 };
 
 module.exports = nextConfig;
