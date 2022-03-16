8ade7812e6bb781ba3e468f01950235e4170633d
ishibashi0112 <a_love.i_w_b_w_y.ya22@ezweb.ne.jp>
2022-03-16 23:11:51 +0900

    🔥

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   images: { domains: ["image.tmdb.org"] },
   env: {
     TMDB_API_KEY: "baa7af1f304682eb49bcbb6db49c1579",
-    // CLERK__FRONTEND_API_KEY: "clerk.divine.vervet-45.lcl.dev",
   },
 };
 
